a2fdaa2142c80f64c75b94bf101e04747103cf5c
MartinezFabian <FabianMartinezDev@gmail.com>
2021-08-25 18:40:43 -0300

    Se agrego un nuevo paquete y clase java

---
+++ src/variables/DeclarandoVariables.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class DeclarandoVariables {
+
+    public static void main(String[] args) {
+
+        String nombre = "Fabian";
+        int edad = 20;
+        float salario = 159.99f;
+        boolean carnet = true;
+
+
+        System.out.println("Nombre: " + nombre + "\nEdad: " + edad + "\nSalario: " + salario + "\nCarnet: " + carnet );
+    }
+}
