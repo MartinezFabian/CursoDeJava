b6817f1645cc7b12acffc9e81f99e59d5c4ed0d8
MartinezFabian <FabianMartinezDev@gmail.com>
2021-09-25 19:28:48 -0300

    Se agrego el desafio un ejercicio combinado

---
+++ src/desafioUnEjercicioCombinado/EjercicioCombinado.java
@@ -0,0 +1,10 @@
+package desafioUnEjercicioCombinado;
+
+public class EjercicioCombinado {
+
+    public static void main(String[] args) {
+        double x = ((6.0/5.0) - ((2.0/3.0) * (7.0/2.0)) + (2.0/30.0)) / ((1.0/3.0)/5.0);
+        System.out.println(x);
+
+    }
+}
