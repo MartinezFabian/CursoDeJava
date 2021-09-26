bc2af433777c402c28d47018c02151b937ce97f5
MartinezFabian <FabianMartinezDev@gmail.com>
2021-09-26 19:45:27 -0300

    Se agrego UsoCuenta.java y CuentaCorriente.java

---
+++ src/poo/desafio/cuentacorriente/CuentaCorriente.java
@@ -0,0 +1,39 @@
+package poo.desafio.cuentacorriente;
+
+public class CuentaCorriente {
+    private String nombreTitular;
+    private long numeroCuenta;
+    private double saldo;
+
+
+    //Constructor
+    public CuentaCorriente(String nombreTitular, double saldo){
+        this.nombreTitular = nombreTitular;
+        this.saldo = saldo;
+        numeroCuenta = (long) (Math.random()*1000000000 + 1);
+    }
+
+    public void transferencia(CuentaCorriente cuenta1, CuentaCorriente cuenta2, double importe){
+        cuenta1.reintegro(importe);
+        cuenta2.ingresar(importe);
+    }
+
+    public String verInformacionDeCuenta() {
+        return "Informacion de cuenta: " +
+                "\nnombreTitular: " + nombreTitular +
+                "\nnumeroCuenta:" + numeroCuenta +
+                "\nsaldo: $" + saldo;
+    }
+
+    public void reintegro(double reintegro){
+        this.saldo = this.saldo - reintegro;
+    }
+
+    public void ingresar(double ingreso) {
+        this.saldo = this.saldo + ingreso;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
---
+++ src/poo/desafio/cuentacorriente/UsoCuenta.java
@@ -0,0 +1,19 @@
+package poo.desafio.cuentacorriente;
+
+public class UsoCuenta {
+    public static void main(String[] args) {
+        CuentaCorriente cuenta1 = new CuentaCorriente("Luis Fernandez", 5000);
+        CuentaCorriente cuenta2 = new CuentaCorriente("Fabricio Dominguez", 2500);
+
+        System.out.println(cuenta1.verInformacionDeCuenta());
+        System.out.println(cuenta2.verInformacionDeCuenta());
+
+        cuenta1.transferencia(cuenta1, cuenta2, 2500);
+
+        System.out.println("\nTransferencia realizada.\n");
+
+        System.out.println(cuenta1.verInformacionDeCuenta());
+        System.out.println(cuenta2.verInformacionDeCuenta());
+
+    }
+}
